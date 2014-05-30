4a74f8d5a8e06d0960339e4e06a9b028361d8be1
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-05-30 15:24:08 -0700

    Main activity calls second activity correctly. Added a second activity, options activity. Fixed title bar issue.

--- PDF-Drive/gen/io/github/nick11roberts/pdf_drive/R.java
+++ PDF-Drive/gen/io/github/nick11roberts/pdf_drive/R.java
@@ -858,10 +858,12 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070005;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int bg_color=0x7f070004;
+        public static final int button_color=0x7f070003;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1091,14 +1093,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1112,12 +1112,18 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int inputTextDocumentTitle=0x7f05003f;
+        public static final int inputTextFolder=0x7f050045;
+        public static final int inputTextNumOfPages=0x7f050042;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int mainButton=0x7f05003c;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int optionsButton=0x7f050046;
+        public static final int optionsDoneButton=0x7f050047;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1138,6 +1144,12 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int tableRowDocumentTitle=0x7f05003d;
+        public static final int tableRowFolder=0x7f050043;
+        public static final int tableRowNumOfPages=0x7f050040;
+        public static final int textView1=0x7f050041;
+        public static final int textView2=0x7f05003e;
+        public static final int textView3=0x7f050044;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1195,8 +1207,9 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_options=0x7f030019;
+        public static final int activity_preview=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1242,10 +1255,15 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a0010;
+        public static final int action_settings=0x7f0a0015;
         public static final int app_name=0x7f0a000d;
         public static final int main_button_upload=0x7f0a000f;
         public static final int main_heading=0x7f0a000e;
+        public static final int options_button_go=0x7f0a0014;
+        public static final int options_folder=0x7f0a0013;
+        public static final int options_heading=0x7f0a0010;
+        public static final int options_pages=0x7f0a0011;
+        public static final int options_title=0x7f0a0012;
     }
     public static final class style {
         /** 
--- PDF-Drive/src/io/github/nick11roberts/pdf_drive/MainActivity.java
+++ PDF-Drive/src/io/github/nick11roberts/pdf_drive/MainActivity.java
@@ -1,65 +1,37 @@
 package io.github.nick11roberts.pdf_drive;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new mainFragment())
-                    .commit();
-        }
-    }
+    
+	    final Button mainButton = (Button) findViewById((Integer) R.id.mainButton);
+	    //final Button optionsButton = (Button) findViewById((Integer) R.id.optionsButton);
+	    
+	    mainButton.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	        	//setContentView(R.layout.activity_options);
+	        	Intent launchOptionsActivityIntent = new Intent(MainActivity.this, OptionsActivity.class);
+	        	startActivity(launchOptionsActivityIntent);
+	        }
+	    });
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class mainFragment extends Fragment {
-
-        public mainFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 
 }
